ebd6e086051123f12aa407019ccbc2e27a7f9586
lthieuuit <56406682+lthieuuit@users.noreply.github.com>
2020-04-27 19:56:22 +0700

    Create project Shopie
    Admin:
    Còn thiếu binding người dùng ra bảng và quản lý sản phẩm.
    Chưa làm giao diện trang bán hàng

---
+++ Shopie/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model.Dao;
+using Model.EF;
+using Shopie.Common;
+
+namespace Shopie.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Admin/User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new UserDao();
+                var encryptedPW = Encrypt.MD5Hash(user.Password);
+                user.Password = encryptedPW;
+
+                long id = dao.Insert(user);
+                if (id > 0)
+                {
+                    return RedirectToAction("Create", "User");           
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Thêm User không thành công");
+                }
+
+            }
+            return View("Index");
+
+        }
+       
+    }
+}
